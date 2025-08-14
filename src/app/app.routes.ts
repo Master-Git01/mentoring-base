77de56eced12c8e82e6920f5f295f449fc9286cb
Plazma06 <example@gmail.com>
2025-08-14 19:46:05 +0300

    fix(routes): Удалил не нужные файлы с дочерними маршрутами, заменил loadChildren на loadComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,16 @@ import { MainPath } from './core/enums/main-path.enum';
 export const routes: Routes = [
   {
     path: MainPath.ROOT,
-    loadChildren: () => import('./main/main.routes').then((m) => m.mainRoutes),
+    loadComponent: () =>
+      import('./main/components/materials-card/materials-card.component').then(
+        (c) => c.MaterialsCardComponent,
+      ),
   },
   {
     path: MainPath.USERS,
-    loadChildren: () =>
-      import('./users/users.routes').then((m) => m.usersRoutes),
+    loadComponent: () =>
+      import('./users/components/users-card/users-card.component').then(
+        (c) => c.UsersCardComponent,
+      ),
   },
 ];
--- src/app/main/main.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const mainRoutes: Routes = [
-  {
-    path: '',
-    loadComponent: () =>
-      import('./pages/main/main.component').then((c) => c.MainComponent),
-  },
-];
