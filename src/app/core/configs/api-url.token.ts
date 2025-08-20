45586b4f60396ad59d27e51508aedd7667a341fd
Plazma06 <example@gmail.com>
2025-08-20 21:17:49 +0300

    feat(core): Добавил интерсептор и Api_Url токен

---
+++ src/app/core/configs/api-url.token.ts
@@ -0,0 +1,5 @@
+import { InjectionToken } from '@angular/core';
+
+export const API_URL = new InjectionToken<string> ('API_URL', {
+  factory: (): string => 'https://jsonplaceholder.typicode.com',
+});
